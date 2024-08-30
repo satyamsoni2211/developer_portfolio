7148b736d23ffa0e54ac2ee935657003b1bd4d45
Satyam Soni <satyamsoni@Satyams-MacBook-Pro.local>
2024-08-30 11:49:40 +0530

    Fixed capcha and email

--- app/components/homepage/contact/contact-without-captcha.jsx
+++ app/components/homepage/contact/contact-without-captcha.jsx
@@ -1,17 +1,18 @@
 "use client";
 // @flow strict
-import { isValidEmail } from '@/utils/check-email';
-import axios from 'axios';
-import { useState } from 'react';
+import { isValidEmail } from "@/utils/check-email";
+// import axios from "axios";
+import emailjs from "@emailjs/browser";
+import { useState } from "react";
 import { TbMailForward } from "react-icons/tb";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
 function ContactWithoutCaptcha() {
   const [error, setError] = useState({ email: false, required: false });
   const [userInput, setUserInput] = useState({
-    name: '',
-    email: '',
-    message: '',
+    name: "",
+    email: "",
+    message: "",
   });
 
   const checkRequired = () => {
@@ -29,28 +30,31 @@ function ContactWithoutCaptcha() {
       return;
     } else {
       setError({ ...error, required: false });
-    };
+    }
 
     const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
     const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
     const options = { publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY };
 
     try {
-      // const res = await emailjs.send(serviceID, templateID, userInput, options);
-      const teleRes = await axios.post(`${process.env.NEXT_PUBLIC_APP_URL}/api/contact`, userInput);
+      const res = await emailjs.send(serviceID, templateID, userInput, options);
+      // const teleRes = await axios.post(
+      //   `${process.env.NEXT_PUBLIC_APP_URL}/api/contact`,
+      //   userInput
+      // );
 
-      if (res.status === 200 || teleRes.status === 200) {
-        toast.success('Message sent successfully!');
+      if (res.status === 200) {
+        toast.success("Message sent successfully!");
         setUserInput({
-          name: '',
-          email: '',
-          message: '',
+          name: "",
+          email: "",
+          message: "",
         });
-      };
+      }
     } catch (error) {
       console.log(error);
       toast.error(error?.text || error);
-    };
+    }
   };
 
   return (
@@ -60,7 +64,9 @@ function ContactWithoutCaptcha() {
       </p>
       <div className="max-w-3xl text-white rounded-lg border border-[#464c6a] p-3 lg:p-5">
         <p className="text-sm text-[#d3d8e8]">
-          {"If you have any questions or concerns, please don't hesitate to contact me. I am open to any work opportunities that align with my skills and interests."}
+          {
+            "If you have any questions or concerns, please don't hesitate to contact me. I am open to any work opportunities that align with my skills and interests."
+          }
         </p>
         <div className="mt-6 flex flex-col gap-4">
           <div className="flex flex-col gap-2">
@@ -70,7 +76,9 @@ function ContactWithoutCaptcha() {
               type="text"
               maxLength="100"
               required={true}
-              onChange={(e) => setUserInput({ ...userInput, name: e.target.value })}
+              onChange={(e) =>
+                setUserInput({ ...userInput, name: e.target.value })
+              }
               onBlur={checkRequired}
               value={userInput.name}
             />
@@ -84,15 +92,19 @@ function ContactWithoutCaptcha() {
               maxLength="100"
               required={true}
               value={userInput.email}
-              onChange={(e) => setUserInput({ ...userInput, email: e.target.value })}
+              onChange={(e) =>
+                setUserInput({ ...userInput, email: e.target.value })
+              }
               onBlur={() => {
                 checkRequired();
                 setError({ ...error, email: !isValidEmail(userInput.email) });
               }}
             />
-            {error.email &&
-              <p className="text-sm text-red-400">Please provide a valid email!</p>
-            }
+            {error.email && (
+              <p className="text-sm text-red-400">
+                Please provide a valid email!
+              </p>
+            )}
           </div>
 
           <div className="flex flex-col gap-2">
@@ -102,18 +114,20 @@ function ContactWithoutCaptcha() {
               maxLength="500"
               name="message"
               required={true}
-              onChange={(e) => setUserInput({ ...userInput, message: e.target.value })}
+              onChange={(e) =>
+                setUserInput({ ...userInput, message: e.target.value })
+              }
               onBlur={checkRequired}
               rows="4"
               value={userInput.message}
             />
           </div>
           <div className="flex flex-col items-center gap-2">
-            {error.required &&
+            {error.required && (
               <p className="text-sm text-red-400">
                 Email and Message are required!
               </p>
-            }
+            )}
             <button
               className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-5 md:px-12 py-2.5 md:py-3 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
               role="button"
@@ -127,6 +141,6 @@ function ContactWithoutCaptcha() {
       </div>
     </div>
   );
-};
+}
 
 export default ContactWithoutCaptcha;
